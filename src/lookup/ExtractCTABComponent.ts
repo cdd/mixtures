f03cc58db02d5e0be71278e883c6d1d6161533a4
Alex Clark <aclark.xyz@gmail.com>
2020-05-07 15:40:16 -0400

    LinkNode enumeration

--- src/lookup/ExtractCTABComponent.ts
+++ src/lookup/ExtractCTABComponent.ts
@@ -45,7 +45,7 @@ export class ExtractCTABComponent
 {
 	public name:string = null;
 	public molecules:wmk.Molecule[] = [];
-			
+
 	// ------------ public methods ------------
 
 	constructor(private text:string)
@@ -103,7 +103,44 @@ export class ExtractCTABComponent
 
 	private enumerateAttachAny(proto:ProtoMolecule):ProtoMolecule[]
 	{
-		return null;
+		if (!proto.attachAny) return null;
+		let bond:number = null;
+		for (let look of proto.attachAny.keys()) {bond = look; break;}
+		if (bond == null) return null;
+		let atoms = proto.attachAny.get(bond);
+		proto.attachAny.delete(bond);
+		if (Vec.isBlank(atoms)) return null;
+
+		let mol = proto.mol;
+		let atomKeep = mol.bondFrom(bond), atomChop = mol.bondTo(bond);
+		if (mol.atomElement(atomChop) == '*') {}
+		else if (mol.atomElement(atomKeep) == '*') [atomKeep, atomChop] = [atomChop, atomKeep];
+		else if (mol.atomAdjCount(atomKeep) < mol.atomAdjCount(atomChop)) [atomKeep, atomChop] = [atomChop, atomKeep];
+
+		// update the baseline prototype in lieu of the atom sacrificial atom getting deleted
+		for (let look of proto.attachAny.keys()) this.removeAtom(proto.attachAny.get(look), atomChop);
+		if (proto.stereoRelative) for (let look of proto.stereoRelative) this.removeAtom(look, atomChop);
+		if (proto.stereoRacemic) for (let look of proto.stereoRacemic) this.removeAtom(look, atomChop);
+		if (proto.linkNodes) for (let n = proto.linkNodes.length - 1; n >= 0; n--)
+		{
+			if (proto.linkNodes[n].atom == atomChop) {proto.linkNodes.splice(n, 1); continue;}
+			if (proto.linkNodes[n].atom > atomChop) proto.linkNodes[n].atom--;
+			this.removeAtom(proto.linkNodes[n].nbrs, atomChop);
+		}
+		if (proto.mixtures) for (let look of proto.mixtures) this.removeAtom(look.atoms, atomChop);
+
+		let list:ProtoMolecule[] = [];
+		for (let connAtom of atoms)
+		{
+			let cmol = mol.clone();
+			if (atomChop == cmol.bondFrom(bond)) cmol.setBondFrom(bond, connAtom); else cmol.setBondTo(bond, connAtom);
+			cmol.deleteAtomAndBonds(atomChop);
+			// TODO: ideally a little redepiction of the connecting bond would be in order, but the connectivity is right, and 
+			// in most cases it will be possible to see what it is
+			list.push(this.protoClone(proto, cmol));
+		}
+
+		return list;
 	}
 
 	// groups of racemic stereocentres: enumerate all combinations (2^group size)
@@ -124,7 +161,7 @@ export class ExtractCTABComponent
 		}
 
 		let nperm = Math.min(256, 1 << bonds.length);
-		let list = [this.shallowClone(proto, mol)];
+		let list = [this.protoClone(proto, mol)];
 		for (let n = 1; n < nperm; n++)
 		{
 			let rmol = mol.clone();
@@ -138,7 +175,7 @@ export class ExtractCTABComponent
 				}
 				bitand = bitand << 1;
 			}
-			list.push(this.shallowClone(proto, rmol));
+			list.push(this.protoClone(proto, rmol));
 		}
 
 		return list;
@@ -161,10 +198,55 @@ export class ExtractCTABComponent
 			else if (bt == wmk.Molecule.BONDTYPE_DECLINED) molinv.setBondType(n, wmk.Molecule.BONDTYPE_INCLINED);
 		}
 
-		return [proto, this.shallowClone(proto, molinv)];
+		return [proto, this.protoClone(proto, molinv)];
 	}
 	private enumerateLinkNodes(proto:ProtoMolecule):ProtoMolecule[]
 	{
+		if (Vec.isBlank(proto.linkNodes)) return null;
+
+		let link = proto.linkNodes.shift();
+
+/*	atom:number;
+	nbrs:number[];
+	minRep:number;
+	maxRep:number;
+*/
+		let mol = proto.mol, a1 = link.atom;
+		let nbr1 = link.nbrs.length >= 1 ? link.nbrs[0] : 0;
+
+		let list:ProtoMolecule[] = [];
+		for (let n = link.minRep; n <= link.maxRep; n++)
+		{
+			if (n == 1)
+			{
+				list.push(this.protoClone(proto, mol));
+				continue;
+			}
+
+			let rmol = mol.clone();
+			for (let i = 2; i <= n; i++)
+			{
+				let a2 = rmol.addAtom(rmol.atomElement(a1), rmol.atomX(a1), rmol.atomY(a1));
+				rmol.setAtomCharge(a2, rmol.atomCharge(a1));
+				rmol.setAtomUnpaired(a2, rmol.atomUnpaired(a2));
+				rmol.addBond(a1, a2, 1);
+				if (nbr1 > 0)
+				{
+					let a3 = 0;
+					for (let adj of rmol.atomAdjList(a1)) if (adj != a2 && adj != nbr1) {a3 = adj; break;}
+					if (a3 > 0)
+					{
+						let b = rmol.findBond(a1, a3);
+						if (rmol.bondFrom(b) == a1) rmol.setBondFrom(b, a2); else rmol.setBondTo(b, a2);
+					}
+				}
+			}
+
+			list.push(this.protoClone(proto, rmol));
+		}
+
+		return list;	
+
 		return null;
 	}
 	private enumerateMixtures(proto:ProtoMolecule):ProtoMolecule[]
@@ -172,18 +254,29 @@ export class ExtractCTABComponent
 		return null;
 	}
 
-	private shallowClone(proto:ProtoMolecule, mol:wmk.Molecule):ProtoMolecule
+	// makes a copy of the prototype's fields, and copies over the replacement molecule
+	private protoClone(proto:ProtoMolecule, mol:wmk.Molecule):ProtoMolecule
 	{
 		let dup:ProtoMolecule =
 		{
 			'mol': mol,
-			'attachAny': proto.attachAny,
-			'stereoRacemic': proto.stereoRacemic,
-			'stereoRelative': proto.stereoRelative,
-			'linkNodes': proto.linkNodes,
-			'mixtures': proto.mixtures,
+			'attachAny': proto.attachAny ? new Map(proto.attachAny) : null,
+			'stereoRacemic': deepClone(proto.stereoRacemic),
+			'stereoRelative': deepClone(proto.stereoRelative),
+			'linkNodes': deepClone(proto.linkNodes),
+			'mixtures': deepClone(proto.mixtures),
 		};
 		return dup;
+	}
+
+	// updates a list given that a certain atom has been deleted: decrements or removes any atoms that are affected
+	private removeAtom(list:number[], atom:number):void
+	{
+		for (let n = list.length - 1; n >= 0; n--)
+		{
+			if (list[n] == atom) list.splice(n, 1);
+			else if (list[n] > atom) list[n]--;
+		}
 	}
 }
 
